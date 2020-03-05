428007ee311d31e8f975ffafd58630a2bf8c251f
HouariZegai <houarizegai14@gmail.com>
2020-03-05 17:02:20 +0100

    play with variables in typescript

---
+++ typescript/variables.js
@@ -0,0 +1,6 @@
+var a; // any type
+a = 10;
+a = "welcome";
+var str; // define type of variable
+str = 'hello';
+console.log(str);
---
+++ typescript/variables.ts
@@ -0,0 +1,9 @@
+let a // any type
+a = 10
+a = "welcome"
+
+
+let str: string // define type of variable
+str = 'hello'
+
+console.log(str);
