31fe8de60627d571c6e59eeb56a7dad5599d1100
HouariZegai <houarizegai14@gmail.com>
2020-04-11 11:12:25 +0100

    Material - Made contextual menu

--- material/src/app/app.module.ts
+++ material/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -28,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
     MatSidenavModule,
     MatToolbarModule
   ],
